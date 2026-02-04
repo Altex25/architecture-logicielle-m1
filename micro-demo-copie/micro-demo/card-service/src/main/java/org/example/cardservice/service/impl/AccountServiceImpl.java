9fd5490846d2dae763e2802ac55a08f4e26ba239
alexandre <alexandre.py25@free.fr>
2026-02-04 14:38:21 +0100

    feat(Loan/Card): Using rest template

--- micro-demo-copie/micro-demo/card-service/src/main/java/org/example/cardservice/service/impl/AccountServiceImpl.java
+++ micro-demo-copie/micro-demo/card-service/src/main/java/org/example/cardservice/service/impl/AccountServiceImpl.java
@@ -3,20 +3,24 @@ package org.example.cardservice.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.example.cardservice.service.AccountService;
 import org.example.cardservice.usable.AccountUsable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 
-@RestController
+@Service
 @RequiredArgsConstructor
 public class AccountServiceImpl implements AccountService {
     private final RestClient restClient = RestClient.create();
+    private final RestTemplate restTemplate;
+
     String BASE_URL = "http://localhost:8090";
 
     @Override
     public AccountUsable getAccountById(Long id) {
-        return restClient.get()
-                .uri(BASE_URL + "/accounts/" + id)
-                .retrieve()
-                .body(AccountUsable.class);
+        return restTemplate.getForObject(BASE_URL + "/accounts/{id}", AccountUsable.class, id);
+//        return restClient.get()
+//                .uri(BASE_URL + "/accounts/" + id)
+//                .retrieve()
+//                .body(AccountUsable.class);
     }
 }
--- micro-demo-copie/micro-demo/loans-service/src/main/java/org/example/loansservice/service/impl/AccountServiceImpl.java
+++ micro-demo-copie/micro-demo/loans-service/src/main/java/org/example/loansservice/service/impl/AccountServiceImpl.java
@@ -3,20 +3,17 @@ package org.example.loansservice.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.example.loansservice.service.AccountService;
 import org.example.loansservice.usable.AccountUsable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-@RestController
+@Service
 @RequiredArgsConstructor
 public class AccountServiceImpl implements AccountService {
-    private final RestClient restClient = RestClient.create();
+    private final RestTemplate restTemplate;
     String BASE_URL = "http://localhost:8090";
 
     @Override
     public AccountUsable getAccountById(Long id) {
-        return restClient.get()
-                .uri(BASE_URL + "/accounts/" + id)
-                .retrieve()
-                .body(AccountUsable.class);
+        return restTemplate.getForObject(BASE_URL + "/accounts/{id}", AccountUsable.class, id);
     }
 }
