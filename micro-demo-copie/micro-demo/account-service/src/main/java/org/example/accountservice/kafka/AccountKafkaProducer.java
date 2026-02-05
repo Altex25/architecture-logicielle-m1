0d9486b7933d20935b93512dd2e015b8a670c59d
alexandre <alexandre.py25@free.fr>
2026-02-05 14:09:13 +0100

    feat(Loan/Card): Add Kafka config

---
+++ micro-demo-copie/micro-demo/account-service/src/main/java/org/example/accountservice/kafka/AccountKafkaProducer.java
@@ -0,0 +1,30 @@
+package org.example.accountservice.kafka;
+
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class AccountKafkaProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public AccountKafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Value("${spring.kafka.topic.account}")
+    private String topic;
+
+    public void sendAccountDelete(Long accountId) {
+        String event = String.format(
+                "{\"event\":\"ACCOUNT_DELETED\",\"accountId\":\"%s\"}",
+                accountId
+        );
+
+        log.info("test");
+
+        kafkaTemplate.send(topic, event);
+    }
+}
