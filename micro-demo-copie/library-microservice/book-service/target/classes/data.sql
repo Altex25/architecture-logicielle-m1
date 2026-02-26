3dea49a18ccb05784df54ca09ede079373d3e6e2
alexandre <alexandre.py25@free.fr>
2026-02-26 16:19:25 +0100

    Delete target directories

--- micro-demo-copie/library-microservice/book-service/target/classes/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO books (id, title, author, isbn, available) VALUES (1, 'Book 1', 'Author 1', '99R8937R39784734', true);
-INSERT INTO books (id, title, author, isbn, available) VALUES (2, 'Book 2', 'Author 2', '9829849742974', true);
--- micro-demo-copie/library-microservice/user-service/target/classes/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (id, name, email) VALUES (1, 'Alice A', 'alice@example.com');
-INSERT INTO users (id, name, email) VALUES (2, 'Bob B', 'bob@example.com');
